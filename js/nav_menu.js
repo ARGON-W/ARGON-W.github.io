67051815cd865988a687ecc3ba5661fd1133f9c2
ARGON-W <tamhanke046@gmail.com>
2024-01-26 00:05:16 +0800

    Site updated: 2024-01-26 00:05:16

--- js/nav_menu.js
+++ js/nav_menu.js
@@ -1,21 +1,26 @@
-// 返回顶部 显示网页阅读进度
-window.onscroll = percent; // 执行函数
-// 页面百分比
-function percent() {
-  let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
-    b =
-      Math.max(
-        document.body.scrollHeight,
-        document.documentElement.scrollHeight,
-        document.body.offsetHeight,
-        document.documentElement.offsetHeight,
-        document.body.clientHeight,
-        document.documentElement.clientHeight
-      ) - document.documentElement.clientHeight, // 整个网页高度 减去 可视高度
-    result = Math.round((a / b) * 100), // 计算百分比
-    btn = document.querySelector("#percent"); // 获取图标
-
-  result <= 99 || (result = 99), (btn.innerHTML = result);
+document.addEventListener('pjax:complete', tonav);
+document.addEventListener('DOMContentLoaded', tonav);
+//响应pjax
+function tonav() {
+    document.getElementById("name-container").setAttribute("style", "display:none");
+    var position = $(window).scrollTop();
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > position) {
+            document.getElementById("name-container").setAttribute("style", "");
+            document.getElementsByClassName("menus_items")[1].setAttribute("style", "display:none!important");
+        } else {
+            document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
+            document.getElementById("name-container").setAttribute("style", "display:none");
+        }
+        position = scroll;
+    });
+    //修复没有弄右键菜单的童鞋无法回顶部的问题
+    document.getElementById("page-name").innerText = document.title.split(" | Fomalhaut🥝")[0];
 }
 
-document.getElementById("page-name").innerText = document.title.split(" | 安知鱼")[0];
+function scrollToTop() {
+    document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
+    document.getElementById("name-container").setAttribute("style", "display:none");
+    btf.scrollToDest(0, 500);
+}
