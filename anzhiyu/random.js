122cd06efd0a69f99d4b1915de10b7f95995efe4
ARGON-W <2297735463@qq.com>
2024-04-23 00:19:36 +0800

    Site updated: 2024-04-23 00:19:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/10/Hexo/hexo/"];function toRandomPost(){
+var posts=["2024/04/10/Hexo/hexo/","2024/04/23/HTML/Html学习笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
